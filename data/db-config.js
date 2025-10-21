5ba68fc33b48a3f3bdeb3774a71f9c73bbeb1ef6
Justin Abellera <justin.c.abellera@gmail.com>
2025-10-21 04:05:32 -0700

    changed environment to production

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const knex = require('knex');
 const configs = require('../knexfile');
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 
 module.exports = knex(configs[environment]);
